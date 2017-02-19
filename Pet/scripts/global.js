ef083ff554cf9fd66380d4592e116330210809dc
konglingnan <konglingnan999@gmail.com>
2017-02-19 17:57:25 +0800

    Pet demo

--- Pet/scripts/global.js
+++ Pet/scripts/global.js
@@ -1,5 +1,5 @@
 // 通用函数开始
-function $(id){
+function $(id) {
 	return document.getElementById(id);
 }
 
@@ -17,7 +17,7 @@ function addLoadEvent(func) {
 }
 
 // 在某个节点后插入新节点
-function insertAfter(newNode, targetNode){
+function insertAfter(newNode, targetNode) {
 	var parent = targetNode.parentNode;
 	if(parent.lastChild == targetNode){
 		parent.appendChild(newNode);
@@ -59,7 +59,7 @@ function highlightPage() {
 // 公共区域结束
 
 // home幻灯片开始
-function prepareSlides(){
+function prepareSlides() {
 	if(!document.createElement) return false;
 	if(!document.createTextNode) return false;
 	if(!document.getElementById) return false;
@@ -69,18 +69,18 @@ function prepareSlides(){
 	var intro = $("intro");
 
 	var div = document.createElement("div");
-	div.setAttribute("id", "placehoderArea");
+	div.setAttribute("id", "placeholderArea");
 	insertAfter(div, intro);
 
 	var img = document.createElement("img");
-	img.setAttribute("id", "placehoder");
+	img.setAttribute("id", "placeholder");
 	img.setAttribute("src", "images/pet.png");
 	img.setAttribute("alt", "Boxes");
 	div.appendChild(img);
 }
 
 // 把一个id为nodeId的元素，移动到(toX, toY)的位置去，每interval毫秒移动余下距离的1/10
-function move(nodeId, toX, toY, interval){
+function move(nodeId, toX, toY, interval) {
 	if(!document.getElementById) return false;
 	if(!document.getElementById(nodeId)) return false;
 
@@ -123,12 +123,12 @@ function move(nodeId, toX, toY, interval){
 }
 
 // 绑定移动事件
-function prepareMove(){
+function prepareMove() {
 	if(!document.getElementsByTagName) return false;
 	if(!document.getElementById) return false;
-	if(!document.getElementById("placehoder")) return false;
+	if(!document.getElementById("placeholder")) return false;
 
-	var placehoder = $("placehoder");
+	var placeholder = $("placeholder");
 	var links = document.getElementsByTagName("a");
 	var here;
 
@@ -139,19 +139,19 @@ function prepareMove(){
 		links[i].onmouseover = function() {
 			here = this.getAttribute("href");
 			if(here.indexOf("home") != -1){
-				move("placehoder", 0, 0, 5);
+				move("placeholder", 0, 0, 5);
 			}
 			if(here.indexOf("about") != -1){
-				move("placehoder", -300, 0, 5);
+				move("placeholder", -300, 0, 5);
 			}
 			if(here.indexOf("photos") != -1){
-				move("placehoder", -600, 0, 5);
+				move("placeholder", -600, 0, 5);
 			}
 			if(here.indexOf("live") != -1){
-				move("placehoder", -900, 0, 5);
+				move("placeholder", -900, 0, 5);
 			}
 			if(here.indexOf("contact") != -1){
-				move("placehoder", -1200, 0, 5);
+				move("placeholder", -1200, 0, 5);
 			}
 		}
 	}
@@ -159,7 +159,7 @@ function prepareMove(){
 // home幻灯片结束
 
 // about介绍开始
-function prepareSection(){
+function prepareSection() {
 	if(!document.getElementById) return false;
 	if(!document.getElementsByTagName) return false;
 	if(!document.getElementsByTagName("article")) return false;
@@ -193,7 +193,7 @@ function showSection(id) {
 // about介绍结束
 
 // photos图片库开始
-function prepareGralley(){
+function prepareGralley() {
 	if(!document.getElementsByTagName) return false;
 	if(!document.getElementById) return false;
 	if(!document.getElementById("gallery")) return false;
@@ -209,7 +209,7 @@ function prepareGralley(){
 	}
 }
 
-function prepareImage(){
+function prepareImage() {
 	if(!document.createElement) return false;
 	if(!document.createTextNode) return false;
 	if(!document.getElementById) return false;
@@ -218,7 +218,7 @@ function prepareImage(){
 	// 创建大图节点和描述节点，并依次添加在缩略图后
 	var gallery = $("gallery");
 	var img = document.createElement("img");
-	img.setAttribute("id", "imgPlacehoder");
+	img.setAttribute("id", "imgPlaceholder");
 	img.setAttribute("src", "images/photos/0.jpg");
 	img.setAttribute("alt", "Pet");
 	insertAfter(img, gallery);
@@ -230,12 +230,12 @@ function prepareImage(){
 	img.parentNode.insertBefore(p, img);
 }
 
-function showPic(node){
-	// 获取节点信息并传给imgPlacehoder和description
-	if(!document.getElementById("imgPlacehoder")) return false;
-	var imgPlacehoder = $("imgPlacehoder");
+function showPic(node) {
+	// 获取节点信息并传给imgPlaceholder和description
+	if(!document.getElementById("imgPlaceholder")) return false;
+	var imgPlaceholder = $("imgPlaceholder");
 	var href = node.getAttribute("href");
-	imgPlacehoder.setAttribute("src", href);
+	imgPlaceholder.setAttribute("src", href);
 
 	if(!document.getElementById("description")) return false;
 	var description = $("description");
@@ -257,7 +257,7 @@ function showPic(node){
 
 // live表格开始
 // 动态添加隔行样式
-function stripeTables(){
+function stripeTables() {
 	if(!document.getElementsByTagName) return false;
 
 	var tables = document.getElementsByTagName("table");
@@ -291,7 +291,11 @@ function prepareHighlight() {
 		}
 	}
 }
-function displayAbbr(){
+function prepareAbbr() {
+	var article = document.getElementsByTagName("article")[0];
+	displayAbbr(article);
+}
+function displayAbbr(node) {
 	// 对象探测
 	if(!document.getElementsByTagName) return false;
 	if(!document.createElement) return false;
@@ -329,9 +333,8 @@ function displayAbbr(){
 	var h1 = document.createElement("h1");
 	var h1Text = document.createTextNode("abbreviations");
 	h1.appendChild(h1Text);
-	var article = document.getElementsByTagName("article")[0];
-	article.appendChild(h1);
-	article.appendChild(dl);
+	node.appendChild(h1);
+	node.appendChild(dl);
 }
 // live表格结束
 
@@ -341,12 +344,129 @@ function focusLabels() {
 	if(!document.getElementById) return false;
 	var labels = document.getElementsByTagName("label");
 	for(var i=0; i<labels.length; i++){
-		if(!document.getAttribute("for")) continue;
+		if(!labels[i].getAttribute("for")) continue;
 		labels[i].onclick = function() {
 			var labelFor = this.getAttribute("for");
 			if(!document.getElementById(labelFor)) return false;
 			var elem = document.getElementById(labelFor);
 			elem.focus();
+		}
+	}
+}
+// 兼容input的placeholder
+function resetFields(form) {
+	if(Modernizr.input.placeholder) return;
+	if(!document.getElementsByTagName) return false;
+	var elems = form.elements;
+	for(var i=0; i<elems.length; i++){
+		var elem = elems[i];
+		if(elem.type == "submit" || elem.type == "reset") continue;
+		var check = elem.placeholder || elem.getAttribute("placeholder");
+		if(!check) continue;
+		elem.onfocus = function() {
+			var text = elem.placeholder || elem.getAttribute("placeholder");
+			if(this.value == text){
+				this.value = "";
+				this.className = "";
+			}
+		}
+		elem.onblur = function() {
+			if(this.value == ""){
+				this.value = this.placeholder || this.getAttribute("placeholder");
+				addClass(this, "holder");
+			}
+		}
+	}
+}
+// 表单验证
+function isText(elem) {
+	if(elem.value.replace(" ", "").length == 0) return false;
+	var holder = elem.placeholder || elem.getAttribute("placeholder");
+	return (elem.value != holder);
+}
+function isEmail(elem) {
+	var check = (elem.value.indexOf("@") != -1) && (elem.value.indexOf(".") != -1);
+	return check;
+}
+function validateForm(form) {
+	var elems = form.elements;
+	for(var i=0; i<elems.length; i++){
+		var elem = elems[i];
+		if(elem.required == "required") {
+			if(!isText(elem)){
+				alert("Please fill in the "+elem.name+"field.");
+				return false;
+			}
+		}
+		if(elem.type == "email") {
+			if(!isEmail(elem)){
+				alert("The "+elem.name+"field must be a valid email address.");
+				return false;
+			}
+		}
+	}
+	return true;
+}
+// ajax刷新提交结果
+function submitFormWithAjax(form, target) {
+	if(window.XMLHttpRequest){
+		var oAjax = new XMLHttpRequest();
+	}else{
+		var oAjax = new ActiveXObject("Microsoft.XMLHttp");
+	}
+	if(!oAjax) return false;
+	displayLoading(target);
+	var dataArr = [];
+	var elem;
+	for(var i=0; i<form.elements.length; i++){
+		var elem = form.elements[i];
+		dataArr[i] = elem.name+"="+encodeURIComponent(elem.value);
+	}
+	var data = dataArr.join("&");
+
+	oAjax.open("POST", form.getAttribute("action"), true);
+
+	// post请求必须有这个头信息
+	oAjax.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+	oAjax.onreadystatechange = function() {
+		if(oAjax.readyState==4){
+			if(oAjax.status==200 || oAjax.status==0){
+				// 匹配<article>与</article>之间的字段，()内为捕获组，返回一个数组，matches[0]是包括<article>标签在内的部分，而matches[1]表示捕获组中模式匹配的部分
+				var matches = oAjax.responseText.match(/<article>([\s\S]+)<\/article>/);
+				if(matches.length>0) {
+					target.innerHTML = matches[1];
+				}else{
+					target.innerHTML = "<p>error!</p>"
+				}
+			}else{
+				target.innerHTML = "<p>"+oAjax.statusText+"</p>";
+			}
+		}
+	};
+	oAjax.send(data);
+	return true;
+}
+function displayLoading(elem) {
+	while(elem.hasChildNodes()) {
+		elem.removeChild(elem.lastChild);
+	}
+	var content = document.createElement("img");
+	content.setAttribute("src", "images/loading.gif");
+	content.setAttribute("alt", "loading...");
+	elem.appendChild(content);
+}
+
+function prepareForms() {
+	var forms = document.getElementsByTagName("form");
+	for(var i=0; i<forms.length; i++){
+		var form = forms[i];
+		resetFields(form);
+		form.onsubmit = function() {
+			if(!validateForm(form)) return false;
+			var article = document.getElementsByTagName("article")[0];
+			if(submitFormWithAjax(form, article)) return false;
+			return true;
 		}
 	}
 }
@@ -360,6 +480,7 @@ addLoadEvent(prepareMove);
 addLoadEvent(highlightPage);
 addLoadEvent(prepareSection);
 addLoadEvent(stripeTables);
-addLoadEvent(displayAbbr)
+addLoadEvent(prepareAbbr);
 addLoadEvent(prepareHighlight);
 addLoadEvent(focusLabels);
+addLoadEvent(prepareForms);
